0417104b74af8c5adf6d43216285999a69dd4367
Andrew Rester <73493592+ar-fullsend@users.noreply.github.com>
2026-05-10 00:09:43 -0500

    Add ShieldOverlay.swift with core shield logic

---
+++ ProofOfConcept/ShieldOverlay.swift
@@ -0,0 +1,15 @@
+// ShieldOverlay.swift
+// PrivacyGuard - System-wide shield implementation
+
+import SwiftUI
+
+class ShieldOverlay {
+    static func activate() {
+        // TODO: Implement system-wide dim/overlay using private APIs or entitlements
+        print("Shield activated - Screen protected")
+    }
+    
+    static func deactivate() {
+        print("Shield deactivated - Normal view restored")
+    }
+}
